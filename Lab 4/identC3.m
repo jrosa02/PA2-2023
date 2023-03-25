785c5dbefd7230a1da7aaf5354854d0dfe3e9f23
Jan Rosa <jrosa@student.agh.edu.pl>
2023-03-25 12:03:02 +0100

    identyfikacja

---
+++ Lab 4/identC3.m
@@ -0,0 +1,14 @@
+function blad = identC(X0)
+load pomiary_3out.mat
+K = X0(1);
+T = X0(2);
+y = pomiary_3out(:,3) - pomiary_3out(1,3);
+global n; 
+t=1:300; 
+for i=1:n %wektor biegunów transmitancji
+    A(i)=-1/T;
+end
+model=zpk([],A,K/T^n); 
+y_sym=step(model,t);
+e = y-y_sym;
+blad = sum(e.^2) / length(e);
